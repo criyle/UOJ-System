4fc858b2e4d721e1a84690d4cf063f733891d404
criyle <gaoyang4425@gmail.com>
2020-06-09 20:20:46 -0400

    feat(web): make blog accessible with port
    - make match pattern with port if port is not 80 http or 443 https

--- web/app/controllers/subdomain/blog/route.php
+++ web/app/controllers/subdomain/blog/route.php
@@ -5,17 +5,19 @@ call_user_func(function() { // to prevent variable scope leak
 	Route::pattern('id', '[1-9][0-9]{0,9}');
 	Route::pattern('blog_username', '[a-zA-Z0-9_\-]{1,20}');
 
+	$port = ((UOJConfig::$data['web']['blog']['protocol'] === "http" && UOJConfig::$data['web']['blog']['port'] == 80) || (UOJConfig::$data['web']['blog']['protocol'] === "https" && UOJConfig::$data['web']['blog']['port'] == 443)) ? '' : (':'.UOJConfig::$data['web']['blog']['port']);
+
 	switch (UOJConfig::$data['switch']['blog-domain-mode']) {
 		case 1:
-			$domain = '{blog_username}.'.UOJConfig::$data['web']['blog']['host'];
+			$domain = '{blog_username}.'.UOJConfig::$data['web']['blog']['host'].$port;
 			$prefix = '';
 			break;
 		case 2:
-			$domain = UOJConfig::$data['web']['blog']['host'];
+			$domain = UOJConfig::$data['web']['blog']['host'].$port;
 			$prefix = '/{blog_username}';
 			break;
 		case 3:
-			$domain = UOJConfig::$data['web']['main']['host'];
+			$domain = UOJConfig::$data['web']['main']['host'].$port;
 			$prefix = '/blog/{blog_username}';
 			break;
 	}
